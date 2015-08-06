92de4f15e9b8e5610bf54bac3e186310cfe3ffc2
Joachim Breitner <mail@joachim-breitner.de>
2015-08-06 13:12:59 +0200

    watch: Process all branches and tags that contain the starting commit

--- tools/ghc/watch.sh
+++ tools/ghc/watch.sh
@@ -3,24 +3,34 @@
 scripts="$(realpath "$(dirname $0)")"
 cd ~/logs/
 
+PATH=/opt/ghc/7.8.4/bin:/opt/alex/3.1.4/bin/:/opt/happy/1.19.5/bin/:$PATH
 
 while true
 do
-	(cd ghc-master; git pull)
-	for rev in $(cd ghc-master; git log --format=%H --first-parent 57ed4101687651ba3de59fb75355f4b83ffdca75..HEAD | tac)
+	git -C ghc-master fetch --prune
+	git -C ghc-master pull
+	for branchtip in $(git -C ghc-master for-each-ref --format='%(objectname)')
 	do
-		if ! [ -e "$rev.log" -o  -e "$rev.log.broken" -o -d "ghc-tmp-$rev" ]
+		if ! git -C ghc-master merge-base --is-ancestor 57ed4101687651ba3de59fb75355f4b83ffdca75 $branchtip
 		then
-			echo "Benchmarking $rev..."
-			$scripts/run-speed.sh "$rev" >/dev/null
-			$scripts/log2json.pl "$rev.log"
-			$scripts/upload.sh "$rev.json"
-
-			git add $rev.log*
-			git commit -m "Log for $rev"
-			git push
-			break
+			continue
 		fi
+
+		for rev in $(git -C ghc-master log --format=%H --first-parent 57ed4101687651ba3de59fb75355f4b83ffdca75..$branchtip | tac)
+		do
+			if ! [ -e "$rev.log" -o  -e "$rev.log.broken" -o -d "ghc-tmp-$rev" ]
+			then
+				echo "Benchmarking $rev..."
+				$scripts/run-speed.sh "$rev" >/dev/null
+				$scripts/log2json.pl "$rev.log"
+				$scripts/upload.sh "$rev.json"
+
+				git add $rev.log*
+				git commit -m "Log for $rev"
+				git push
+				break
+			fi
+		done
 	done
 	sleep 60 || break
 done
